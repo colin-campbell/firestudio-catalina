3d88c3642ed4f43e2afc28709b372813429f4993
Colin Campbell <colin.campbell@digitalistgroup.com>
2020-05-23 00:25:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+## Run unsupported PreSonus FireStudio* on Mac OS Catalina 10.15
+
+### NO SUPPORT WHATSOEVER IS PROVIDED FOR THIS! IF IT EATS YOUR FIRSTBORN, THEN IT IS ON YOU.
+
+This script will help install the Firewire driver and Universal Control v1.7.6  
+for unsupported PreSonus Firewire devices on Mac OS Catalina 10.15  
+
+What the script does:
+* Downloads and installs the new Catalina-compatible Firewire driver from UC v3.2.1-57667
+* Downloads the old UC Control Panel app from v 1.7.6-5875
+* Extracts the control panel and panel drivers from the old version
+* Installs `Universal Control.app` and the panel drivers in the correct locations
+* Fixes permissions on the installed software.
+
+### Instructions
+* Download the script: `firestudio_catalina.sh`
+* Run the script in a terminal with: `sh presonus_catalina.sh` you will be prompted for your password.
+* Reboot when finished.
+
+To uninstall, run the PreSonus uninstaller from the **new** dmg:  
+ `PreSonus_UniversalControl_v3_2_1_57677.dmg`
+
+Mac OS Gatekeeper may complain about the new Firewire driver if you have SIP enabled, if prompted, Visit System Preferences->Security & Privacy to allow the drievr to run.  
+
+You may need to power-cycle the interface once after the driver is installed. 
+
+
+
---
+++ firestudio_catalina.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+DRIVER_DMG="PreSonus_UniversalControl_v3_2_1_57677.dmg"
+UC_DMG="PreSonus_Universal_Control-5875.dmg"
+
+
+REMOTE_DRIVER="https://pae-web.presonusmusic.com/downloads/products/dmg/${DRIVER_DMG}"
+REMOTE_UC="https://pae-web.presonusmusic.com/downloads/products/dmg/${UC_DMG}"
+SUBDIR="Presonus Universal Control"
+FW_DRIVER="${SUBDIR}/PreSonus FireWire Driver.pkg"
+
+echo
+echo "Downloading Firewire driver."
+curl -O -# "${REMOTE_DRIVER}"
+
+
+printf "\nDownloading Control Panel\n"
+curl -O -# "{$REMOTE_UC}"
+
+
+printf "\nMounting Firewire driver dmg"
+MOUNT_DRIVE=$(hdiutil attach -noverify -mountroot ./ ${DRIVER_DMG} | awk 'FNR==1{print $1; exit}')
+
+
+printf "\nInstalling Firewire driver\n\n"
+sudo installer -pkg "$FW_DRIVER" -target /
+
+
+printf "\nEjecting Firewire driver dmg\n"
+hdiutil detach "${MOUNT_DRIVE}"
+
+printf "\nMounting Control panel dmg\n"
+MOUNT_DRIVE=$(hdiutil attach -noverify -mountroot ./ ${UC_DMG} | awk 'FNR==1{print $1; exit}')
+
+printf "\nExpanding Control panel Package\n"
+pkgutil --expand-full "${SUBDIR}/PreSonus Universal Control.pkg" tmp
+
+printf "\nEjecting Control panel dmg\n"
+hdiutil detach "${MOUNT_DRIVE}"
+
+printf "\nRemoving Universal Control.app if it exists.\n"
+sudo rm -rf "/Applications/Universal Control.app"
+
+printf "\nCopying Universal Control.app to /Applications\n"
+sudo cp -a "tmp/UCInstaller.pkg/Payload/Applications/Universal Control.app" /Applications/
+
+printf "\nFixing permissions on Universal Control.app\n"
+sudo chown -R root:admin "/Applications/Universal Control.app"
+
+printf "\nEnsure Devices directory exists.\n"
+sudo mkdir -p "/Library/Application Support/Presonus/Devices"
+
+printf "\nCopying device drivers to correct location\n"
+sudo cp -a  "tmp/UCInstaller.pkg/Payload/Library/Application Support/Presonus/Devices" "/Library/Application Support/Presonus/" 
+
+printf "\nFixing permissions on device drivers\n"
+sudo chown -R root:wheel "/Library/Application Support/Presonus"
+
+printf "\nRemoving temporary files\n"
+rm -rf tmp
+
+printf "\n\nAll done! reboot your computer to load the new software."
