429c55692d84d406fb0d7870fae40570482a79db
Colin Campbell <colin.campbell@digitalistgroup.com>
2023-01-03 21:42:51 +0100

    Update build to use new notarytool

--- build.sh
+++ build.sh
@@ -3,22 +3,10 @@
 set -u
 
 VERSION="$1"
-USERNAME="$ASC_USERNAME"
-PASSWORD="$ASC_PASSWORD"
+
 CERT="$CERT_NAME"
 
-DMG="firestudio-catalina-sur-${VERSION}.dmg"
-# create temporary log files
-NOTARIZE_APP_LOG=$(mktemp -t notarize-app)
-NOTARIZE_INFO_LOG=$(mktemp -t notarize-info)
-
-
-# Delete temporary files on exit
-function finish {
-	rm "$NOTARIZE_APP_LOG" "$NOTARIZE_INFO_LOG"
-  rm -rf tmp
-}
-trap finish EXIT
+DMG="firestudio-catalina-sur-ventura-${VERSION}.dmg"
 
 ## Increment build number.
 OLD_BUILD_NUMBER=$(/usr/libexec/PlistBuddy -c "Print :CFBundleVersion" Firestudio\ Catalina.app/Contents/Info.plist)
@@ -39,28 +27,5 @@ cp -a Firestudio\ Catalina.app README.md CHANGELOG.txt ./tmp/
 hdiutil create -volname "Firestudio Catalina $VERSION" -srcfolder ./tmp -ov -format UDZO "$DMG"
 
 ## Submit DMG for notarization
-if xcrun altool --notarize-app --primary-bundle-id se.digitalist.fscatalina -u "$USERNAME" -p "$PASSWORD"  -f "$DMG" > "$NOTARIZE_APP_LOG" 2>&1; then
-  cat "$NOTARIZE_APP_LOG"
-	RequestUUID=$(awk -F ' = ' '/RequestUUID/ {print $2}' "$NOTARIZE_APP_LOG")
-
-# check status periodically
-	while sleep 60 && date; do
-		# check notarization status
-		if xcrun altool --notarization-info "$RequestUUID"  -u "$USERNAME" -p "$PASSWORD" > "$NOTARIZE_INFO_LOG" 2>&1; then
-			cat "$NOTARIZE_INFO_LOG"
-
-			# once notarization is complete, run stapler and exit
-			if ! grep -q "Status: in progress" "$NOTARIZE_INFO_LOG"; then
-				xcrun stapler staple "$DMG"
-				exit $?
-			fi
-		else
-			cat "$NOTARIZE_INFO_LOG" 1>&2
-			exit 1
-		fi
-	done
-
-else
-	cat "$NOTARIZE_APP_LOG" 1>&2
-	exit 1
-fi  
+xcrun notarytool submit --keychain-profile "DGDeveloperID" --wait --progress "$DMG"
+xcrun stapler staple "$DMG"
